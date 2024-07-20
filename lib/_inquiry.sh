179da8944edb7e5fd082011b96189b2e67356ee2
msoliveira <msoliveira01@hotmail.com>
2024-07-19 23:28:49 -0300

    Update _inquiry.sh

--- lib/_inquiry.sh
+++ lib/_inquiry.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-get_frontend_url() {
-  print_banner
-  printf "${WHITE} 💻 Digite o domínio da interface web (Frontend):${GRAY_LIGHT}"
-  printf "\n\n"
-  read -p "> " frontend_url
-}
-
-get_backend_url() {
-  print_banner
-  printf "${WHITE} 💻 Digite o domínio da sua API (Backend):${GRAY_LIGHT}"
-  printf "\n\n"
-  read -p "> " backend_url
-}
-
-get_urls() {
-  get_frontend_url
-  get_backend_url
-}
-
 izing_atualizar() {
   git_update
   backend_node_dependencies
